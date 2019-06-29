0f082c755c42fdfca4abd0c4b5d2bc3b59883ff1
freckledpapaya <maya.helferty@gmail.com>
2019-06-29 15:11:37 -0700

    Migrate basic user table, write user model auth methods and validations

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true 
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def User.find_by_credentials(email, pw)
+        user = User.find_by(email: email)
+
+        return nil unless user
+        return nil unless user.is_password?(pw)
+        user
+    end
+
+    def User.generate_session_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.session_token).is_password?(pw)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token 
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def password=(pw)
+        @password = pw
+        self.session_token = BCrypt::Password.create(pw)
+    end
+
+
+end

876cab5c1f32976e2da34bb335acf3408bd0d959
App Academy Student <student@appacademy.io>
2019-07-02 14:57:17 -0700

    Setup frontend file tree, initialize app and root files with basic content

---
+++ frontend/actions/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import components here
+
+import { Route } form 'react-router-dom';
+// AuthRoute/ProtectedRoute here
+
+export default () => (
+  <div>
+  // Routes here
+  </div>
+);
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -8,5 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const preloadedState = {};
     const store = createStore(preloadedState);
+    window.store = store;
     ReactDOM.render(<h2>React is running</h2>, root)
 });
